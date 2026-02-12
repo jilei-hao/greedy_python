0c3b2d551b69fdb4e9b50242ce6d602467ef91bc
Paul Yushkevich <pyushkevich@gmail.com>
2026-02-12 16:41:39 -0500

    Update macOS conditionals in prebuild.sh

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -18,11 +18,11 @@ if [[ $1 =~ ubuntu-.* ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.tar.xz
   LIB_SUFFIX=so
   MAKEFLAGS="-- -j 8"
-elif [[ $1 == macos-13 ]]; then
+elif [[ $1 =~ macos-.*-intel ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_10_10_x86_64.tar.xz
   LIB_SUFFIX=dylib
   MAKEFLAGS="-- -j 8"
-elif [[ $1 == macos-14 ]]; then
+elif [[ $1 =~ macos-.* ]]; then
   VTK_BINARY=vtk-wheel-sdk-9.3.1-cp310-cp310-macosx_11_0_arm64.tar.xz
   LIB_SUFFIX=dylib
   MAKEFLAGS="-- -j 8"
